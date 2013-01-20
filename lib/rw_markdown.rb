03b85a306bf0c5278f4a9e6eda14b285744d9f15
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-01-20 09:03:51 +0200

    audio js

--- lib/rw_markdown.rb
+++ lib/rw_markdown.rb
@@ -21,13 +21,45 @@ module RwMarkdown
   
   module Helper
     def audio_tag(options = {})
-      "<audio class=\"podcast_player\" src=\"#{current_page.data.audio_url}\" preload=\"none\" />" if current_page && current_page.data && current_page.data.audio_url
+      if current_page && current_page.data && current_page.data.audio_url
+        str = <<EOS
+        <div>
+          <audio class="podcast_player" src="#{current_page.data.audio_url}" preload="none" />
+        </div>
+        <div class="track-details">#{current_page.title} (#{current_page.data.duration}), #{number_to_human_size(current_page.data.audio_length)}</div>
+EOS
+        str
+      end
+    end
+    
+    def number_to_human_size(number, precision = 2)
+      number = begin
+        Float(number)
+      rescue ArgumentError, TypeError
+        return number
+      end
+      case
+        when number.to_i == 1 then
+          "1 Byte"
+        when number < 1024 then
+          "%d Bytes" % number
+        when number < 1048576 then
+          "%.#{precision}f KB"  % (number / 1024)
+        when number < 1073741824 then
+          "%.#{precision}f MB"  % (number / 1048576)
+        when number < 1099511627776 then
+          "%.#{precision}f GB"  % (number / 1073741824)
+        else
+          "%.#{precision}f TB"  % (number / 1099511627776)
+      end.sub(/([0-9]\.\d*?)0+ /, '\1 ' ).sub(/\. /,' ')
+    rescue
+      nil
     end
   end
   
   module RwMarkdownRenderer
     def paragraph(text)
-      return "<p>#{middleman_app.audio_tag()}</p>" if "[audio_tag]" == text
+      return middleman_app.audio_tag() if "[audio_tag]" == text
       "<p>#{text}</p>"
     end
   end
