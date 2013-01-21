f9d9f403178a74c97f8a00a4181a88ede3239441
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-01-21 12:41:32 +0200

    fix requirements

--- config.rb
+++ config.rb
@@ -123,7 +123,7 @@ configure :build do
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  require "middleman-smusher"
+  #require "middleman-smusher"
   #activate :smusher
 
   # Or use a different image path
