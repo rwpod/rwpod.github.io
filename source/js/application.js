bde7b79e261e29633aa2bf6482491fa081766ab8
Alex Chaplinsky <alchapone@yandex.ru>
2014-10-17 10:35:05 +0700

    More responsiveness

--- source/js/application.js
+++ source/js/application.js
@@ -2,8 +2,5 @@
 //=require ./vendors/modernizr-2.8.2.min
 //=require ./vendors/audio
 
-//=require ./vendors/foundation/foundation
-//=require ./vendors/foundation/foundation.topbar
-
 //=require ./rwpod_settings
 //=require ./rwpod
