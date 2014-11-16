394203c00614c3df959c0cd77fc0554257072401
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-11-16 13:42:28 +0200

    player

--- lib/rw_markdown.rb
+++ lib/rw_markdown.rb
@@ -51,47 +51,56 @@ module RwMarkdown
 
       if data && data.audio_url && article
         str = <<-END.split("\n").map!(&:strip).join("")
-<div class="podcast_player jp-jplayer" data-url="#{data.audio_url}" data-title="#{article.title}">
-  <audio preload="metadata" src="#{data.audio_url}" title="#{article.title}"></audio>
-</div>
+<div>
 
-<div class="player_interface jp-audio">
-	<div class="jp-type-single">
-		<div class="jp-gui jp-interface">
-			<ul class="jp-controls">
-				<li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
-				<li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
-				<li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
-				<li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
-				<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
-				<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
-				<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
-				<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
-			</ul>
-			<div class="jp-progress">
-				<div class="jp-seek-bar">
-					<div class="jp-play-bar"></div>
-				</div>
-			</div>
-			<div class="jp-volume-bar">
-				<div class="jp-volume-bar-value"></div>
-			</div>
-			<div class="jp-time-holder">
-				<div class="jp-current-time"></div>
-				<div class="jp-duration"></div>
-			</div>
-			<ul class="jp-toggles">
-				<li><a href="javascript:;" class="jp-shuffle" tabindex="1" title="shuffle">shuffle</a></li>
-				<li><a href="javascript:;" class="jp-shuffle-off" tabindex="1" title="shuffle off">shuffle off</a></li>
-				<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
-				<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
-			</ul>
-		</div>
-		<div class="jp-no-solution">
-			<span>Update Required</span>
-			To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-		</div>
-	</div>
+  <div class="podcast_player jp-jplayer" data-url="#{data.audio_url}" data-title="#{article.title}">
+    <audio preload="metadata" src="#{data.audio_url}" title="#{article.title}"></audio>
+  </div>
+
+  <div class="player_interface jp-audio">
+    <div class="jp-type-single">
+      <div class="jp-gui jp-interface">
+        <ul class="jp-controls">
+          <li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
+          <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+          <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+          <li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
+          <li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+          <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
+          <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
+          <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+        </ul>
+
+        <div class="jp-progress">
+          <div class="jp-seek-bar">
+            <div class="jp-play-bar"></div>
+          </div>
+        </div>
+
+        <div class="jp-volume-bar">
+          <div class="jp-volume-bar-value"></div>
+        </div>
+
+        <div class="jp-time-holder">
+          <div class="jp-current-time"></div>
+          <div class="jp-duration"></div>
+        </div>
+
+        <ul class="jp-toggles">
+          <li><a href="javascript:;" class="jp-shuffle" tabindex="1" title="shuffle">shuffle</a></li>
+          <li><a href="javascript:;" class="jp-shuffle-off" tabindex="1" title="shuffle off">shuffle off</a></li>
+          <li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
+          <li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
+        </ul>
+      </div>
+
+      <div class="jp-no-solution">
+        <span>Update Required</span>
+        To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+      </div>
+    </div>
+  </div>
+
 </div>
 
 <div class="track-details">
