d121a8468f9692a11891eb79ca10ac3ab241c8df
Alexey Vasiliev <leopard.not.a@gmail.com>
2020-12-22 01:10:11 +0200

    cleanup + news

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ const cssLoaders = [
   {
     loader: 'sass-loader',
     options: {
-      sourceMap:      true,
+      sourceMap: true,
       webpackImporter: true,
       implementation: require('sass'),
       sassOptions: {
