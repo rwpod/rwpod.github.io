ec25d45f24639b650932fbba3a7b33690ed36353
Alexey Vasiliev <leopard.not.a@gmail.com>
2020-12-30 16:15:50 +0200

    lazy load comments

--- webpack/disqus.js
+++ webpack/disqus.js
@@ -5,7 +5,13 @@ const DISQUS_SHORTNAME = 'rwpod'
 
 /* Disqus */
 
+const OBSERVER_THRESHOLD = 0.5
+
+let disqusEmbedLoaded = false
 let disqusCounterLoaded = false
+let disqusObserver = null
+
+const discusThreadElement = () => document.getElementById('disqus_thread')
 
 const initDisqusScript = (type = 'embed') => {
   const script = document.createElement('script')
@@ -21,6 +27,15 @@ const initDisqusScript = (type = 'embed') => {
   parrent.appendChild(script)
 }
 
+const initDisqusEmbed = () => {
+  if (disqusEmbedLoaded) {
+    return
+  }
+
+  initDisqusScript()
+  disqusEmbedLoaded = true
+}
+
 const initDisqusCounter = () => {
   if (disqusCounterLoaded) {
     return
@@ -30,14 +45,73 @@ const initDisqusCounter = () => {
   disqusCounterLoaded = true
 }
 
+const toggleDisqusScript = (entry) => {
+  entry.forEach((change) => {
+    if (change.intersectionRatio >= OBSERVER_THRESHOLD) {
+      initDisqusEmbed()
+    }
+  })
+}
+
+const initIntersectionObserver = () => {
+  if (!discusThreadElement()) {
+    return
+  }
+
+  if (!window.IntersectionObserver) {
+    initDisqusEmbed()
+    return
+  }
+
+  cleanIntersectionObserver()
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: OBSERVER_THRESHOLD
+  }
+
+  disqusObserver = new window.IntersectionObserver(toggleDisqusScript, observerOptions)
+  disqusObserver.observe(discusThreadElement())
+}
+
+const cleanIntersectionObserver = () => {
+  if (!discusThreadElement() || !disqusObserver) {
+    return
+  }
+
+  disqusObserver.unobserve(discusThreadElement())
+  disqusObserver.disconnect()
+  disqusObserver = null
+}
+
+
 onDomReady(() => {
   initDisqusCounter()
+  initIntersectionObserver()
 
   if (Turbolinks.supported) {
     document.addEventListener('turbolinks:load', () => {
       if (window.DISQUSWIDGETS && window.DISQUSWIDGETS.getCount) {
-        window.DISQUSWIDGETS.getCount({reset: true})
+        window.DISQUSWIDGETS.getCount({
+          reset: true
+        })
       }
+
+      initIntersectionObserver()
+
+      if (discusThreadElement() && window.DISQUS && window.DISQUS.reset) {
+        window.DISQUS.reset({
+          reload: true,
+          config: () => {
+            this.page.identifier = document.title
+            this.page.url = location.href
+          }
+        })
+      }
+    })
+    document.addEventListener('turbolinks:before-cache', () => {
+      cleanIntersectionObserver()
     })
   }
 })
