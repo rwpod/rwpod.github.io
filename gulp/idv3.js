d9b699fa4412c8ff2bd3d73c5b3651bf6cac97dc
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-04-06 02:59:52 +0300

    update

--- gulp/idv3.js
+++ gulp/idv3.js
@@ -9,16 +9,51 @@ const NodeID3 = require('node-id3')
 const knownOptions = {
   string: 'env',
   default: {env: process.env.NODE_ENV || 'production'}
-};
+}
 
 const options = minimist(process.argv.slice(2), knownOptions)
 
 /**
  * Run in terminal
- * ./node_modules/.bin/gulp update_idv3 --audio /Users/leo/Stuff/podcasts/08/0801/0801.mp3 --md source/posts/2020/01-13-podcast-08-01.html.md --number 0801
+ * ./node_modules/.bin/gulp get_tags --md source/posts/2020/01-13-podcast-08-01.html.md
  *
  */
-gulp.task('update_idv3', (cb) => {
+
+gulp.task('get_tags', (cb) => {
+  const podcast = matter(fs.readFileSync(options.md, 'utf8'))
+  const description = htmlToText.htmlToText(marked(podcast.content), {
+    wordwrap: false,
+    ignoreHref: true,
+    ignoreImage: true
+  }).replace('READMORE', '')
+
+  /* eslint-disable no-console */
+  console.log('Title:')
+  console.log(podcast.data.title)
+  console.log('')
+  console.log('Artist:')
+  console.log('Алексей Васильев')
+  console.log('')
+  console.log('Album:')
+  console.log('RWpod - подкаст про мир Ruby и Web технологии')
+  console.log('')
+  console.log('Comment:')
+  console.log(description)
+  console.log('')
+  console.log('Genre:')
+  console.log('Podcast')
+  console.log('')
+  /* eslint-enable no-console */
+
+  return cb()
+})
+
+/**
+ * Run in terminal
+ * ./node_modules/.bin/gulp mp3_tags --audio /Users/leo/Stuff/podcasts/08/0801/0801.mp3 --md source/posts/2020/01-13-podcast-08-01.html.md --number 0801
+ *
+ */
+gulp.task('mp3_tags', (cb) => {
   const podcast = matter(fs.readFileSync(options.md, 'utf8'))
   const description = htmlToText.htmlToText(marked(podcast.content), {
     wordwrap: false,
