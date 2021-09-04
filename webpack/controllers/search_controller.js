06be63f1e6c4fefbb77e8d25acc04976059e55cb
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-09-04 22:58:14 +0300

    fixes

--- webpack/controllers/search_controller.js
+++ webpack/controllers/search_controller.js
@@ -174,6 +174,6 @@ export default class extends Controller {
       '</div>',
       `<div class="search-box-container--item-content">${document.content}</div>`,
       '</a>'
-    ].join(' ')
+    ].join('')
   }
 }
