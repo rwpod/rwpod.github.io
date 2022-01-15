126de402ce50c1c4da397195ad2165588872b8ae
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-01-15 22:41:08 +0200

    start use new helpers

--- config.rb
+++ config.rb
@@ -78,7 +78,8 @@ activate :external_pipeline,
          command: "yarn run assets:#{build? ? 'build' : 'watch'}",
          source: assets_dir,
          latency: 2,
-         ignore_exit_code: true
+         ignore_exit_code: true,
+         manifest_json: File.expand_path('.assets-build/manifest.json', __dir__)
 
 activate :auto_blank_links,
          ignore_hostnames: ['rwpod.com', 'www.rwpod.com']
