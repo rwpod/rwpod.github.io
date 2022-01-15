9c360f607077ce9e1c1ef447eede905853cf312c
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-01-15 13:59:14 +0200

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const TerserPlugin = require('terser-webpack-plugin')
-const {default: MiniCssExtractPlugin} = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 const WebpackAssetsManifest = require('webpack-assets-manifest')
