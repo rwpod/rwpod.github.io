5c692b2dc2f8db131c54160c47b5669312a07f17
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-11-21 01:29:26 +0200

    Start migration to Astro

--- src/pages/rss/styles.xsl.js
+++ src/pages/rss/styles.xsl.js
@@ -2,13 +2,13 @@ import stylesheetContent from './styles.xsl.liquid?raw'
 import { renderLiquid } from '@utils/liquid'
 import { DEFAULT_TITLE, DEFAULT_DESCRIPTION, DEFAULT_AUTHOR } from '@utils/helpers'
 
-export const get = async () => {
-  const body = await renderLiquid(stylesheetContent, {
-    title: DEFAULT_TITLE,
-    site: import.meta.env.SITE,
-    description: DEFAULT_DESCRIPTION,
-    author: DEFAULT_AUTHOR
-  })
+const body = await renderLiquid(stylesheetContent, {
+  title: DEFAULT_TITLE,
+  site: import.meta.env.SITE,
+  description: DEFAULT_DESCRIPTION,
+  author: DEFAULT_AUTHOR
+})
 
+export const get = async () => {
   return { body }
 }
