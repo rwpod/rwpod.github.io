8873bcb3311f06458673f4323f2f781090829fe0
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-11-27 12:02:22 +0200

    improve mobile expirience

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,7 +39,8 @@ export default defineConfig({
       'icon-192.png',
       'icon-512.png',
       'maskable_icon.png',
-      'plyr.svg'
+      'plyr.svg',
+      'images/logo.svg'
     ],
     injectManifest: {
       globPatterns: ['**/*.{css,js}']
